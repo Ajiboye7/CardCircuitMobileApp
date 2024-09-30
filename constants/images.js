b70178e7511e4444964d383149414e9495add980
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-09-30 18:52:15 +0100

    'update'

---
+++ constants/images.js
@@ -0,0 +1,9 @@
+import logo from "../assets/images/Logo.png"
+import graphics from "../assets/images/graphics.png"
+import RightbuttomImage from "../assets/images/Ellipse 2.png"
+
+export default{
+    logo,
+    graphics,
+    RightbuttomImage
+}
