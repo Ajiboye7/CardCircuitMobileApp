35de501f5b1ba3e666650e3b17671dffe3507e33
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-10-01 17:38:51 +0100

    'index'

--- app/index.jsx
+++ app/index.jsx
@@ -6,13 +6,23 @@ import {images} from "../constants"
 
 const index = () => {
   return (
-   <SafeAreaView className="bg-primary">
-   <ScrollView>
+   <SafeAreaView className="bg-primary h-full">
+   <ScrollView contentContainerStyle={{height:"100%"}}>
     <View>
     <Image
     source={images.indexImage}
     resizeMode='contain'
     />
+    <View>
+      <Text className="text-white">
+      Simplify Your Wallet.
+      Unify Your Finances.
+      </Text>
+      <Text className="text-white">
+      Welcome to seamless centralized banking.
+      Manage and track all your cards in one app. 
+      </Text>
+    </View>
     </View>
     </ScrollView>
    </SafeAreaView>
