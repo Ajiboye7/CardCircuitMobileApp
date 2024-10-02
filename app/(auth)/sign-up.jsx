4ddfc815305d1d965f0615c8cf224ba0f37d9002
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-10-02 16:46:58 +0100

    signup

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,11 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { ScrollView } from 'react-native-web'
 
 const signUp = () => {
   return (
-    <View>
-      <Text>signUp</Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
