171a39fe19b4ecfa25e33cfd2177358374f6502d
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-10-02 16:47:44 +0100

    style

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,20 +24,7 @@ module.exports = {
       fontWeight: {
         510: '510', 
         590: '590', 
-        600: '600',
-        700: '700', 
-        400: '400', 
-      },
-      fontFamily:{
-        sfProRoundedBlack:["SF-Pro-Rounded-Black, san-serif"],
-        sfProRoundedBold:["SF-Pro-Rounded-Bold, san-serif"],
-        sfProRoundedHeavy:["SF-Pro-Rounded-Heavy, san-serif"],
-        sfProRoundedLight:["SF-Pro-Rounded-Light, san-serif"],
-        sfProRoundedMedium:["SF-Pro-Rounded-Medium, san-serif"],
-        sfProRoundedRegular:["SF-Pro-Rounded-Regular, san-serif"],
-        sfProRoundedSemibold:["SF-Pro-Rounded-Semibold, san-serif"],
-        sfProRoundedThin:["SF-Pro-Rounded-Thin, san-serif"],
-        sfProRoundedUltralight:["SF-Pro-Rounded-UltraLIght, san-serif"],
+        600: '600',-Rounded-UltraLIght, san-serif"],
         sfPro:["SF-Pro, san-serif"],
         sfProItalic:["SF-Pro-Italic, san-serif"],
       }
