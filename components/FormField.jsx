cfcd1dab10d0f0d06b93e68ce39603a57dd73ed9
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-10-03 16:53:20 +0100

    commit

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -2,16 +2,19 @@ import { View, Text,TextInput } from 'react-native'
 import React from 'react'
 
 
-const FormField = ({title, value, placeholder, handleChangeText,keyboardType}) => {
+const FormField = ({title, otherStyles, value, placeholder, handleChangeText,keyboardType}) => {
   return (
-    <View className="">
-      <Text className="text-fontColor-gray">{title}</Text>
-      <View className="bg-white w-[360px] h-[52px] rounded-[10px] border ">
+    <View className={`mt-5 ${otherStyles}`}>
+      <Text className="text-fontColor-gray text-[16px] mb-2">{title}</Text>
+      <View className="bg-white w-[360px] h-[52px] rounded-[10px] border justify-center items-start p-5">
         <TextInput
             value={value}
             placeholder={placeholder}
+            placeholderTextColor="#858585"
+            className="text-[16px]"
             onChangeText={handleChangeText}
             keyboardType={keyboardType}
+
         />
       </View>
     </View>
