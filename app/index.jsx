767d968480b0da8f90a72cce929d924e85012213
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-10-04 12:39:24 +0100

    update

--- app/index.jsx
+++ app/index.jsx
@@ -16,31 +16,6 @@ const Onboarding = () => {
     source={images.indexImage}
     resizeMode='contain'
     />
-    </View>
-    
-    <View className="h-1/2 mt-10">
-    <Text className="text-white text-[32px] leading-[40px]">
-        <Text>Simplify Your</Text>
-        <Text className="font-bold"> Wallet.</Text>{'\n'}
-        <Text className="font-bold">Unify</Text> {""}
-        <Text>Your </Text>
-        <Text>Finances.</Text>
-      </Text>
-      <Text className="text-white my-8 text-[14px] leading-[20px]">
-      Welcome to seamless centralized banking.{'\n'}
-      Manage and track all your cards in one app. 
-      </Text>
-      <CustomButton
-        title="Get Started"
-        handlePress={()=> router.push('/sign-up')}
-      />
-      <View className="flex-row gap-2 pt-5">
-          <Text className="text-lg text-gray-100 font-sfPro">Already have an account?</Text>
-          <Link href="/home" className="text-secondary text-lg font-psemibold">Log In </Link>
-        </View>
-    </View>
-    </View>
-    </ScrollView>
    </SafeAreaView>
   )
 }
