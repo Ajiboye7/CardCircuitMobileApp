1375652198bf4d2a15047cfa00ca5b8f81278979
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-10-04 12:40:50 +0100

    screen

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -39,4 +39,11 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack
+      <Stack>
+       <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      </Stack>
+    </ThemeProvider>
+  );
+}
