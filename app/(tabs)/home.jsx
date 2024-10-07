cb380e451c89c89b3cf51c2b276e7184ef2058a7
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-10-07 12:05:27 +0100

    notification

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -7,24 +7,23 @@ const Home = () => {
   return (
     <SafeAreaView>
     <ScrollView>
-      <View>
-      <View className="flex flex-row items-center">
-      <View className="bg-secondary w-[40px] h-[40px] rounded-full p-3 justify-center items-center ">
+      <View className="flex flex-row items-center justify-between px-3">
+      <View className="flex flex-row items-center relative">
+      <View className="bg-secondary z-10 w-[40px] h-[40px]  rounded-full p-3 justify-center items-center">
           <Image source={icons.profile}
            className="w-[30px] h-[30px]"/>
         </View>
+     
         <View className="relative">
-        <Text className="text-white bg-secondary p-2 w-[140px] h-[36px]">
+        <Text className="text-white bg-secondary p-2 w-[140px] h-[36px] text-center -ml-2">
           Username
         </Text>
         {/* Inverted curve using absolute positioning */}
-        <View className="absolute bg-primary w-[20px] h-[36px] rounded-r-full right-32 top-0"></View>
+        <View className="absolute bg-primary w-[25px] h-[36px] rounded-r-full right-32 top-0"></View>
       </View>
     </View>
-    
+    <Image source={icons.notification} className="w-[30px] h-[30px]" />
       </View>
-        <Image source={icons.notification} className="w-[30px] h-[30px]" />
-      
     </ScrollView>
   </SafeAreaView>
   );
