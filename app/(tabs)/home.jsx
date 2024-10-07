48409a0f9d3963b573090923b82fcfaa87207989
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-10-07 10:48:53 +0100

    home

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,12 +1,25 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Image } from "react-native-web";
+import { icons } from "../../constants";
 
 const Home = () => {
   return (
-    <View>
-      <Text className="text-white">Home</Text>
-    </View>
-  )
-}
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+          <View>
+            <Image source={icons.profile} />
+            <Text className="text-white">Username</Text>
+          </View>
+          <Image
+            source={icons.notification}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 export default Home;
