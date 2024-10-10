a0d17c9312fd11ea4eb7d2cca922788cbf8e63ae
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-10-10 16:48:42 +0100

    commit

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -48,35 +48,6 @@ const Home = () => {
             resizeMode="cover"
             className="h-[243px] justify-center items-center"
           >
-            {/*<View className="bg-tertiary-30 w-[313px] h-[180px] rounded-[20px] relative">
-              <View className="justify-center items-center flex-1 gap-5">
-                <View className="bg-secondary w-[20px] h-[20px] items-center justify-center rounded-full p-4">
-                  <Image source={icons.add} />
-                </View>
-
-                <Text className="text-white text-[14px] font-sfPro">
-                  Add a new Card
-                </Text>
-              </View>
-
-
-              <TouchableOpacity 
-              onPress={handleClick}
-              activeOpacity={0.7}
-               >
-              <View className=" w-[30px] h-[30px] absolute bottom-5 right-[297px] bg-secondary rounded-full items-center justify-center">
-                <Image source={icons.arrowLeft} className="w-[40px] h-[40px]" />
-              </View>
-              </TouchableOpacity>
-
-              
-              <View className="w-[30px] h-[30px] absolute bottom-5 left-[297px] bg-secondary rounded-full items-center justify-center">
-                <Image
-                  source={icons.arrowRight}
-                  className="w-[40px] h-[40px]"
-                />
-              </View>
-            </View>*/}
 
             <View className="bg-tertiary-30 w-[313px] h-[180px] rounded-[20px] relative justify-center items-center">
 
