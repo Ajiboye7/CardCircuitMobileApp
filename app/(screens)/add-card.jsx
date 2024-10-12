00df9ac68d2862da9e856f763e0e2c924f9d9c5f
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-10-12 12:25:36 +0100

    formield

--- app/(screens)/add-card.jsx
+++ app/(screens)/add-card.jsx
@@ -19,16 +19,31 @@ const AddCard = () => {
         </ImageBackground>
 
         <Text className="text-white text-[32px] font-sfProRoundedBold">Add Card Details</Text>
-        <View>
+        <View className="w-[360px]">
+        
           <FormField
             placeholder="**** **** **** 325"
             title="Enter Card Number"
           />
 
-          <View>
-            <FormField/>
-            <FormField/>
+          <View className="flex flex-row items-center justify-between">
+            <FormField
+              title="Expiry Date"
+              placeholder="07/23"
+              inputStyles="w-[151px]"
+            />
+            <FormField
+              title="CCV"
+              placeholder="****"
+              inputStyles="w-[151px]"
+            />
           </View>
+
+          <FormField
+            title="Enter Card Alias"
+            placeholder="e.g Primary Savings"
+
+          />
         </View>
       </View>
     </ScrollView>
--- components/FormField.jsx
+++ components/FormField.jsx
@@ -2,11 +2,11 @@ import { View, Text,TextInput } from 'react-native'
 import React from 'react'
 
 
-const FormField = ({title, otherStyles, value, placeholder, handleChangeText,keyboardType}) => {
+const FormField = ({title, otherStyles, value, placeholder, handleChangeText,keyboardType, inputStyles}) => {
   return (
     <View className={`mt-5 ${otherStyles}`}>
       <Text className="text-fontColor-gray text-[16px] mb-2 ">{title}</Text>
-      <View className= {`bg-white p-4 w-[360px] h-[52px] rounded-[10px] border justify-center items-start${inputStyles}`}>
+      <View className= {`bg-white p-4 w-[360px] h-[52px] rounded-[10px] border justify-center items-start ${inputStyles}`}>
         <TextInput
             value={value}
             placeholder={placeholder}
