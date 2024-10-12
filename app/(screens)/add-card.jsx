064c60809ed0b2963c80025425352f38d1773c16
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-10-12 13:21:05 +0100

    scroll

--- app/(screens)/add-card.jsx
+++ app/(screens)/add-card.jsx
@@ -1,4 +1,4 @@
-import { View, Text,ScrollView, ImageBackground} from 'react-native'
+import { View, Text,ScrollView, ImageBackground,Image} from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { icons, images } from '../../constants'
@@ -10,18 +10,17 @@ const AddCard = () => {
   return (
    <SafeAreaView>
     <ScrollView>
-      <View>
+      <View className="relative">
         <ImageBackground
         source={images.indexImage}
         resizeMode='cover'
         className='w-[393px] h-[328px]'
         >
-
         </ImageBackground>
-
         <Text className="text-white text-[32px] font-sfProRoundedBold">Add Card Details</Text>
-        <View className="w-[360px]">
 
+
+        <View className="w-[360px]">
           <FormField
             placeholder="**** **** **** 325"
             title="Enter Card Number"
@@ -50,9 +49,15 @@ const AddCard = () => {
             icon={icons.arrowRight}
           />
           </View>
-          
 
         </View>
+
+        <View className="absolute right-0 bottom-0 top-[720px] w-[100px] h-[100px]">
+          <Image
+            source={images.RightbuttomImage}
+            className=""
+          />
+          </View> 
       </View>
     </ScrollView>
    </SafeAreaView>
