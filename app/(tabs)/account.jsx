2a0f58b963978b22eda61089bc1e9a55e0d8a258
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-10-15 15:06:37 +0100

    switch

--- app/(tabs)/account.jsx
+++ app/(tabs)/account.jsx
@@ -1,12 +1,57 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView,Image, Switch } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {React, useState }from "react";
+import { icons } from "../../constants";
+
 
 const account = () => {
-  return (
-    <View>
-      <Text className="text-white">account</Text>
-    </View>
-  )
-}
 
-export default account
+  const [isEnabled, setIsEnabled] = useState(false);
+
+  // Function to toggle the switch
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+  return (
+    <SafeAreaView className="mx-3 mt-5">
+      <ScrollView>
+      <View className="flex flex-row justify-between items-center">
+        <View className="flex gap-1">
+          <Text className="text-white text-[32px] font-sfProRoundedBold">My Account,</Text>
+          <Text className="text-white text-[12px]">Username</Text>
+        </View>
+        <Image 
+          source={icons.profile2}
+        />
+      </View>
+
+      <View  className="flex flex-row justify-between items-center mb-3 text-[14px]" >
+        <Text className="text-white">Enable Finger Print/Face ID</Text>
+        <Switch
+        trackColor={{ false: '#767577', true: '#81b0ff' }}
+        thumbColor={isEnabled ? '#3555F5' : '#f4f3f4'}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
+      </View>
+
+      <View className="flex flex-row justify-between items-center mb-3 text-[14px]" >
+        <Text className="text-white">Show Dashboard Account Balance</Text>
+        <Switch
+        trackColor={{ false: '#767577', true: '#81b0ff' }}
+        thumbColor={isEnabled ? '#3555F5' : '#f4f3f4'}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
+      </View> 
+
+
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default account;
+
+
