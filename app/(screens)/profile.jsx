1fb5a402c313ab678be4da80bc9a8834b62f5461
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-10-15 19:27:41 +0100

    refresh

--- app/(screens)/profile.jsx
+++ app/(screens)/profile.jsx
@@ -3,51 +3,57 @@ import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import IconButton from "../../components/IconButton";
 import { icons } from "../../constants";
+import CustomButton from "../../components/CustomButton";
 
 const Profile = () => {
   return (
     <SafeAreaView>
       <ScrollView>
-        <View className="flex flex-row justify-between items-center mt-5">
+        <View className="flex flex-row justify-between items-center mt-5 mx-3">
           <View className="bg-secondary w-[30px] h-[30px] rounded-full">
             <Image source={icons.arrowLeft} />
           </View>
           <Image source={icons.profile2} />
         </View>
 
-        <Text className="text-white text-[32px] font-sfProRoundedBold my-5">
+        <Text className="text-white text-[32px] font-sfProRoundedBold my-5 mx-3">
           My Profile
         </Text>
-
-        <View className="flex flex-row justify-between items-center">
-          <Text className="text-white text-[14px]">User ID</Text>
-          <Text className="text-white text-[14px]">ee29euf0d29eu29</Text>
+        <View className='flex flex-row justify-between items-center mb-4 mx-3'>
+            <Text className='text-white'>USer ID</Text>
+            <Text className='text-white'>ee29euf0d29eu29</Text>
         </View>
-
-        <View className=" bg-tertiary-20 flex gap-4 ">
-          <View className="flex flex-row justify-between items-center border-b border-fontColor-gray pb-5">
+        <View className=" bg-tertiary-20 h-[433px] mx-2 px-3 pt-5 rounded-[20px]">
+          <View className="flex flex-row justify-between items-center border-b border-fontColor-gray pb-5 mb-5">
             <Text className="text-white text-fontColor-light-90">
               Display Picture
             </Text>
             <Image source={icons.profile2} />
           </View>
 
-          <View className="flex flex-row justify-between items-center border-b border-fontColor-gray pb-5">
+          <View className="flex flex-row justify-between items-center border-b border-fontColor-gray pb-5 mb-5">
             <Text className="text-white text-fontColor-light-90">Name</Text>
-            <Text className="text-white">Ajiboye Muyideen</Text>
+            <Text className="text-white font-sfProRoundedBold">Ajiboye Muyideen</Text>
           </View>
 
-          <View className="flex flex-row justify-between items-center border-b border-fontColor-gray pb-5">
+          <View className="flex flex-row justify-between items-center border-b border-fontColor-gray pb-5 mb-5">
             <Text className="text-white text-fontColor-light-90">Email</Text>
-            <Text className="text-white">ajiboyemuyideen7@gmail.com</Text>
+            <Text className="text-white font-sfProRoundedBold">ajiboyemuyideen7@gmail.com</Text>
           </View>
 
-          <View className="flex flex-row justify-between items-center border-b border-fontColor-gray pb-5">
+          <View className="flex flex-row justify-between items-center border-b border-fontColor-gray pb-5 mb-5">
             <Text className="text-white text-fontColor-light-90">
               Phone Number
             </Text>
-            <Text className="text-white">08172710973</Text>
+            <Text className="text-white font-sfProRoundedBold">08172710973</Text>
           </View>
+
+          <View className="justify-center items-center mt-8">
+          <CustomButton 
+            title="Edit Profile"
+          />
+          </View>
+         
         </View>
       </ScrollView>
     </SafeAreaView>
--- components/CustomButton.jsx
+++ components/CustomButton.jsx
@@ -15,33 +15,36 @@ const CustomButton = ({handlePress,textStyles,title,containerStyles}) => {
 
 export default CustomButton*/
 
+import { Text, TouchableOpacity, View, Image } from "react-native";
+import React from "react";
 
-import { Text, TouchableOpacity, View, Image } from 'react-native';
-import React from 'react';
-
-const CustomButton = ({ handlePress, title, textStyles, containerStyles, icon }) => {
+const CustomButton = ({
+  handlePress,
+  title,
+  textStyles,
+  containerStyles,
+  icon,
+}) => {
   return (
     <View>
       <TouchableOpacity
-      onPress={handlePress}
-      activeOpacity={0.7}
-      className={`bg-secondary rounded-[10px] h-[50px] w-[146px] justify-center items-center flex-row ${containerStyles}`}
-    >
+        onPress={handlePress}
+        activeOpacity={0.7}
+        className={`bg-secondary rounded-[10px] h-[50px] w-[146px] justify-center items-center flex-row ${containerStyles}`}
+      >
+        <View className="flex flex-row items-center justify-center">
+          <Text
+            className={`text-white font-590 text-[16px] font-sfPro ${textStyles}`}
+          >
+            {title}
+          </Text>
 
-    <View className="flex flex-row items-center justify-center">
-    <Text className={`text-white font-590 text-[16px] font-sfPro ${textStyles}`}>
-        {title}
-      </Text>
-
-      {/* Render the icon if it exists */}
-      {icon && (
-        <Image source={icon} className="w-[30px] h-[30px]" />
-      )}
+          {/* Render the icon if it exists */}
+          {icon && <Image source={icon} className="w-[30px] h-[30px]" />}
+        </View>
+      </TouchableOpacity>
     </View>
-    </TouchableOpacity>
-    </View>
-    
   );
-}
+};
 
 export default CustomButton;
