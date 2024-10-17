f2275af64e902572619b30184326eccbe4616722
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-10-17 20:21:38 +0100

    context Api error

--- app/(screens)/profile.jsx
+++ app/(screens)/profile.jsx
@@ -15,7 +15,29 @@ const Profile = () => {
           </View>
           <Image source={icons.profile2} />
         </View>
-t-white text-fontColor-light-90">Email</Text>
+
+        <Text className="text-white text-[32px] font-sfProRoundedBold my-5 mx-3">
+          My Profile
+        </Text>
+        <View className='flex flex-row justify-between items-center mb-4 mx-3'>
+            <Text className='text-white'>USer ID</Text>
+            <Text className='text-white'>ee29euf0d29eu29</Text>
+        </View>
+        <View className=" bg-tertiary-20 h-[433px] mx-2 px-3 pt-5 rounded-[20px]">
+          <View className="flex flex-row justify-between items-center border-b border-fontColor-gray pb-5 mb-5">
+            <Text className="text-white text-fontColor-light-90">
+              Display Picture
+            </Text>
+            <Image source={icons.profile2} />
+          </View>
+
+          <View className="flex flex-row justify-between items-center border-b border-fontColor-gray pb-5 mb-5">
+            <Text className="text-white text-fontColor-light-90">Name</Text>
+            <Text className="text-white font-sfProRoundedBold">Ajiboye</Text>
+          </View>
+
+          <View className="flex flex-row justify-between items-center border-b border-fontColor-gray pb-5 mb-5">
+            <Text className="text-white text-fontColor-light-90">Email</Text>
             <Text className="text-white font-sfProRoundedBold">ajiboyemuyideen7@gmail.com</Text>
           </View>
 
