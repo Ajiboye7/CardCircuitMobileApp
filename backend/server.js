d23d9e69e45af219e1bdddd83af740bb4a081e66
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-11-01 19:33:50 +0100

    arrange code

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,25 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const express = require('express');
-const mongoose = require('mongoose');
-const userRoute = require('./routes/userRoute');
+const express = require("express");
+const mongoose = require("mongoose");
+const userRoute = require("./routes/userRoute");
 
 const app = express();
 
-app.use(express.json()); 
+app.use(express.json());
 
-app.use('/user', userRoute);
+app.use("/user", userRoute);
 
-
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(process.env.PORT, () => {
-      console.log('Connected to DataBase & listening on port', process.env.PORT);
+      console.log(
+        "Connected to DataBase & listening on port",
+        process.env.PORT
+      );
     });
   })
   .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
+    console.error("Error connecting to MongoDB:", error);
   });
