7f32820b48db2a29460ef118df9c1c7797925854
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-11-02 15:42:11 +0100

    Login error

--- context/useSignUp.js
+++ context/useSignUp.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const useSignUp = () => {
-  const { dispatch } = useAuth(); 
+  const { dispatch } = useAuth();
 
   const register = async ({ name, email, password, retypePassword }) => {
     dispatch({ type: "SET_LOADING" });
