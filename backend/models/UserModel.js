21992e3a0ba12a38ab0719040eac6dce3ad8b25b
Ajiboye <ajiboyemuyideen7@gmail.com>
2025-02-20 20:51:36 +0100

    updated code

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -5,7 +5,25 @@ const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
   name: {
-    type: String
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+/*const userSchema = new Schema({
+  name: {
     type: String,
     required: true,
   },
