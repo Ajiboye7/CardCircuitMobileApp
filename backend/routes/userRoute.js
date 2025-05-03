c3d958aedcd316c9eb1bf09ed440382358de635f
Ajiboye <ajiboyemuyideen7@gmail.com>
2025-05-03 14:43:48 +0100

    updated

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,1 +1,11 @@
 const { signInUser, signUpUser } = require("../controller/userController");
+
+const express = require("express");
+
+const router = express.Router();
+
+
+router.post("/signIn", signInUser);
+router.post("/signUp", signUpUser);
+
+module.exports = router;
